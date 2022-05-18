38eb408e57bcc1c82c34900c6e8c128688ec641f
LeonID <lmatskevich79@gmail.com>
2022-05-18 13:27:12 +0300

    feat: node-stdio

--- 03-node-stdio/test.js
+++ 03-node-stdio/test.js
@@ -1,21 +1,34 @@
 // console.log('Hello, Node.js!');
 
-// Задание 1
-// const { stdin, stdout } = process;
-// stdout.write('Как тебя зовут?\n');
-// stdin.on('data', (data) => {
-//   stdout.write('Привет, ');
-//   stdout.write(data);
-//   process.exit();
-// });
+/* Задание 1
+Напишите программу, которая спрашивает у пользователя его имя, после ввода имени приветствует его, указывая имя, а затем прекращает свою работу и прощается с пользователем. */
+const { stdin, stdout } = process;
+stdout.write('Как тебя зовут?\n');
+stdin.on('data', (data) => {
+  stdout.write('Привет, ');
+  stdout.write(data);
+  process.exit();
+});
 
 // process.on('exit', () => stdout.write('Удачи!'));
 
 // Buffer
 
-const { stdin, stdout } = process;
-stdin.on('data', (data) => {
-  const dataStringified = data.toString();
-  stdout.write('Сообщение в верхнем регистре: ');
-  stdout.write(dataStringified.toUpperCase());
+// const { stdin, stdout } = process;
+// stdin.on('data', (data) => {
+//   const dataStringified = data.toString();
+//   stdout.write('Сообщение в верхнем регистре: ');
+//   stdout.write(dataStringified.toUpperCase());
+// });
+
+/* Задание 2
+Напишите программу, которая спрашивает у пользователя его имя, после ввода имени возвращает указанное пользователем имя наоборот и прекращает работу. */
+
+// const { stdin, stdout } = process;
+stdout.write('Как тебя зовут?\n');
+stdin.on('data', data => {
+  const dataReverse = data.reverse();
+  stdout.write(' ')
+  stdout.write(dataReverse);
+  process.exit();
 });
